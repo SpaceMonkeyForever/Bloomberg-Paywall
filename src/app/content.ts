b14e2f7f4aab97236a2abaf266e16c1c8b63d480
Space Monkey <spacemonkeysltd@gmail.com>
2020-03-14 12:25:49 +0000

    listen for element addition to execute script

--- src/app/content.ts
+++ src/app/content.ts
@@ -3,23 +3,43 @@ chrome.runtime.sendMessage({}, (response) => {
         if (document.readyState === "complete") {
             clearInterval(checkReady)
             console.log("Running Bloomberg paywall extension!")
-
-            removePayWall(document)
-            enableScrolling(document)
+            listenForChildElementAndRemove()
         }
     })
 })
 
 
-function removePayWall(d: Document) {
-    const ePaywallId = "graphics-paywall-overlay"
-    const ePaywall = d.getElementById(ePaywallId)
-    ePaywall.remove()
-}
 
 function enableScrolling(d: Document) {
     const attribute = "data-paywall-overlay-status"
     const selector = `[${attribute}*="show"]`
     const elements = [...document.querySelectorAll(selector)]
     elements.forEach(e => e.setAttribute(attribute, "hide"))
+}
+
+function listenForChildElementAndRemove() {
+    const targetNode = document.body
+    const ePaywallId = "graphics-paywall-overlay"
+
+    // Options for the observer (which mutations to observe)
+    const config = { childList: true };
+
+    // Callback function to execute when mutations are observed
+    const observe = function(mutationsList, observer) {
+        // Use traditional 'for loops' for IE 11
+        for(let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                mutation.addedNodes.forEach(childElement => {
+                    if (childElement.id == ePaywallId) {
+                        childElement.remove()
+                        enableScrolling(document)
+                        // observer.disconnect()
+                    }
+                })
+            }
+        }
+    };
+
+    const observer = new MutationObserver(observe);
+    observer.observe(targetNode, config);
 }
