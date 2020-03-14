b9e21e0c3c349908d92d0127b03d03183c51b0ec
Space Monkey <spacemonkeysltd@gmail.com>
2020-03-14 11:44:37 +0000

    remove bloomberg paywall

--- src/app/content.ts
+++ src/app/content.ts
@@ -2,7 +2,24 @@ chrome.runtime.sendMessage({}, (response) => {
     var checkReady = setInterval(() => {
         if (document.readyState === "complete") {
             clearInterval(checkReady)
-            console.log("We're in the injected content script!")
+            console.log("Running Bloomberg paywall extension!")
+
+            removePayWall(document)
+            enableScrolling(document)
         }
     })
 })
+
+
+function removePayWall(d: Document) {
+    const ePaywallId = "graphics-paywall-overlay"
+    const ePaywall = d.getElementById(ePaywallId)
+    ePaywall.remove()
+}
+
+function enableScrolling(d: Document) {
+    const attribute = "data-paywall-overlay-status"
+    const selector = `[${attribute}*="show"]`
+    const elements = [...document.querySelectorAll(selector)]
+    elements.forEach(e => e.setAttribute(attribute, "hide"))
+}
