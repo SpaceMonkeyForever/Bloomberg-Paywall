36bbba22ce511082fef1a5cc0c22264587957fda
Space Monkey <spacemonkeysltd@gmail.com>
2020-12-22 16:34:54 +0000

    perform action on popup display or manually

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,69 +1,59 @@
-const delayToRunMs = 3000
+// https://developer.chrome.com/docs/extensions/mv2/messaging/
+chrome.runtime.onMessage.addListener((request) => {
+    console.log("received message: " + request)
 
-chrome.runtime.sendMessage({}, (response) => {
-    var checkReady = setInterval(() => {
-        if (document.readyState === "complete") {
-            clearInterval(checkReady)
-            console.log("Running Bloomberg paywall extension!")
-            
-            try {
-                // simple way instead of listening to the element being added
-                setTimeout(runAll, delayToRunMs)
-            } catch (error) {
-                
-            }
+    if (request.action === 'case1') {
+        PaywallRemover.case1()
+    }
+
+    if (request.action === 'case2') {
+        PaywallRemover.case2()
+    }
+});
+
+class PaywallRemover
+{
+    static runBothCases() {
+        PaywallRemover.case1()
+        PaywallRemover.case2()
+    }
+
+    static case1() {
+        PaywallRemover.removePaywallElement1()
+        PaywallRemover.showHiddenArticle()
+    }
+
+    static case2() {
+        PaywallRemover.removePaywallElement2()
+        PaywallRemover.enableScrolling()
+    }
+
+    // There are two elements, either can appear
+    static removePaywallElement1() {
+        const ePaywall2Class = "paywall-inline-tout"
+        const es = document.getElementsByClassName(ePaywall2Class)
+        for (let i = 0; i < es.length; i++) {
+            const e = es[i];
+            e.remove()
         }
-    })
-})
-
-function runAll() {
-    method1()
-    method2()
-}
-
-function method1() {
-    try {
-        removePaywallElement1()
-        enableScrolling()
     }
-    catch(error) {}
-}
 
-function method2() {
-    try {
-        removePaywallElement2()
-        showHiddenArticle()
-    }
-    catch(error) {}
-}
-
-// There are two elements, either can appear
-const ePaywall1Id = "graphics-paywall-overlay"
-const ePaywall2Class = "paywall-inline-tout"
-
-function removePaywallElement1() {
-    const e = document.getElementById(ePaywall1Id)
-    e.remove()
-}
-
-function removePaywallElement2() {
-    const es = document.getElementsByClassName(ePaywall2Class)
-    for (let i = 0; i < es.length; i++) {
-        const e = es[i];
+    static removePaywallElement2() {
+        const ePaywall1Id = "graphics-paywall-overlay"
+        const e = document.getElementById(ePaywall1Id)
         e.remove()
     }
-}
 
+    static enableScrolling() {
+        const attribute = "data-paywall-overlay-status"
+        const selector = `[${attribute}*="show"]`
+        const elements = [...document.querySelectorAll(selector)]
+        elements.forEach(e => e.setAttribute(attribute, "hide"))
+    }
 
-function enableScrolling() {
-    const attribute = "data-paywall-overlay-status"
-    const selector = `[${attribute}*="show"]`
-    const elements = [...document.querySelectorAll(selector)]
-    elements.forEach(e => e.setAttribute(attribute, "hide"))
-}
-
-function showHiddenArticle() {
-    const selector = `p[style*="display: none;"]`
-    const elements = [...document.querySelectorAll(selector)]
-    elements.forEach(e => e.setAttribute("style", ""))
+    static showHiddenArticle() {
+        const selector = `p[style*="display: none;"]`
+        const elements = [...document.querySelectorAll(selector)]
+        elements.forEach(e => e.setAttribute("style", ""))
+    }
 }
--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -3,19 +3,41 @@ import * as ReactDOM from "react-dom"
 
 import "../styles/popup.css"
 
-class Hello extends React.Component {
+class RemoveActions extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // This binding is necessary to make `this` work in the callback
+        this.sendMessageToContentPage = this.sendMessageToContentPage.bind(this);
+    }
+
+    sendMessageToContentPage(message: String) {
+        chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+            console.log("sending case1 to content");
+            // https://developer.chrome.com/docs/extensions/mv2/messaging/
+            chrome.tabs.sendMessage(activeTabs[0].id, { action: message });
+        });
+    }
+
     render() {
+        console.log("rendering")
         return (
             <div className="popup-padded">
-                <h1>{ chrome.i18n.getMessage("l10nHello") }</h1>
+                <h1>Manual:</h1>
+                <button onClick={() => this.sendMessageToContentPage("case1")}> Case 1 </button>
+                <button onClick={() => this.sendMessageToContentPage("case2")}> Case 2 </button>
             </div>
         )
     }
 }
 
-// --------------
+// perform action once popup is displayed i.e. when the extension button is clicked
+chrome.tabs.query({ active: true, currentWindow: true}, function(activeTabs) {
+    chrome.tabs.sendMessage(activeTabs[0].id, { action: "case1" });
+    chrome.tabs.sendMessage(activeTabs[0].id, { action: "case2" });
+});
 
 ReactDOM.render(
-    <Hello />,
+    <RemoveActions />,
     document.getElementById('root')
 )
